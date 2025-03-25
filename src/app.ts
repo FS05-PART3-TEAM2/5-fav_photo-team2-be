65d29102f0f5fc5b4519109a31bd500cecc0fe26
seha <hansaemj20@gmail.com>
2025-03-25 17:37:51 +0900

    task: swagger 초기 설정

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./domains/routes";
+import { swaggerUi, swaggerSpec } from './utils/swagger/swagger';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use("/api", routes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
