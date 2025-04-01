28af61a03592cc9e325d62b0d2d5573d8f5a1e6a
seha <hansaemj20@gmail.com>
2025-04-01 15:26:21 +0900

    task: mock 데이터 및 seed 생성 추가

---
+++ prisma/seeds/randombox.seed.ts
@@ -0,0 +1,150 @@
+import { userIDs } from "./user.seed";
+
+export const randomBoxDraws = [
+  {
+    id: "84b38958-a28f-4a3d-9f9d-4ef1cff06f7f",
+    userId: userIDs[0],
+    earnedPoints: 200,
+  },
+  {
+    id: "0702cac2-fd9c-477c-b871-540f97788042",
+    userId: userIDs[1],
+    earnedPoints: 50,
+  },
+  // ... 나머지 28개
+  {
+    id: "c0ebbb7d-6d91-4a61-b6f7-5f49ea1967df",
+    userId: userIDs[2],
+    earnedPoints: 500,
+  },
+  {
+    id: "e2552097-e7ed-4042-8f25-a8146c8fcb7f",
+    userId: userIDs[3],
+    earnedPoints: 30,
+  },
+  {
+    id: "2231cad2-5cea-43b9-b5fe-04b8cea250c1",
+    userId: userIDs[4],
+    earnedPoints: 100,
+  },
+  {
+    id: "cf19dd32-4bc2-4ffb-8dcb-66b2f0a6faad",
+    userId: userIDs[5],
+    earnedPoints: 70,
+  },
+  {
+    id: "aed7b838-c5f6-48b5-9a31-edf9a3ccf453",
+    userId: userIDs[6],
+    earnedPoints: 200,
+  },
+  {
+    id: "d019864a-637c-4f61-bc65-201edf1d79dc",
+    userId: userIDs[7],
+    earnedPoints: 60,
+  },
+  {
+    id: "343281e7-28f8-44bc-9e6e-d921627a02a9",
+    userId: userIDs[8],
+    earnedPoints: 300,
+  },
+  {
+    id: "fd191929-bf32-4e68-a14f-62e3b77dcdfc",
+    userId: userIDs[9],
+    earnedPoints: 40,
+  },
+  {
+    id: "b63bab33-a154-4889-9c8a-8cdd5ca5d0f3",
+    userId: userIDs[10],
+    earnedPoints: 80,
+  },
+  {
+    id: "51da2751-3a3b-47f6-bc4c-5c6189f785a2",
+    userId: userIDs[11],
+    earnedPoints: 120,
+  },
+  {
+    id: "90bb7c61-c5a7-4a6f-8ffc-032753df037c",
+    userId: userIDs[12],
+    earnedPoints: 500,
+  },
+  {
+    id: "c1a8c194-4202-443b-9d9e-d3b60f60a5cb",
+    userId: userIDs[13],
+    earnedPoints: 100,
+  },
+  {
+    id: "b805ffde-2bc3-4eda-8d2f-6356b44ed525",
+    userId: userIDs[14],
+    earnedPoints: 210,
+  },
+  {
+    id: "13a71558-3593-41ad-b577-8a12656ff926",
+    userId: userIDs[15],
+    earnedPoints: 30,
+  },
+  {
+    id: "54b455cd-4dc0-4e22-98bb-09d3f1f425ff",
+    userId: userIDs[16],
+    earnedPoints: 300,
+  },
+  {
+    id: "25fabca2-f347-4515-8ab8-373dc58d8d5c",
+    userId: userIDs[17],
+    earnedPoints: 150,
+  },
+  {
+    id: "b6f4c696-17f0-4988-ae79-5f6e6bebe516",
+    userId: userIDs[18],
+    earnedPoints: 90,
+  },
+  {
+    id: "9af5f1c8-118e-4340-a83c-4f6a0a93aae9",
+    userId: userIDs[19],
+    earnedPoints: 400,
+  },
+  {
+    id: "ed0f91c6-0178-4fc0-a052-42031e6f5ee0",
+    userId: userIDs[0],
+    earnedPoints: 60,
+  },
+  {
+    id: "39bdc3c6-c570-470c-8f14-c595939cbb84",
+    userId: userIDs[1],
+    earnedPoints: 250,
+  },
+  {
+    id: "39d770cf-7f07-420b-9b6c-1c1ebe53782c",
+    userId: userIDs[2],
+    earnedPoints: 70,
+  },
+  {
+    id: "924c45ea-d05d-45ee-a3ef-d46a4cfa04cd",
+    userId: userIDs[3],
+    earnedPoints: 200,
+  },
+  {
+    id: "bbd19e85-4b1d-4ebb-a2b6-bd15a2ebdf84",
+    userId: userIDs[4],
+    earnedPoints: 350,
+  },
+  {
+    id: "28afca52-33bc-4a08-9e5a-72a71540283a",
+    userId: userIDs[5],
+    earnedPoints: 40,
+  },
+  {
+    id: "205d888c-51ab-426a-93df-ce0661bbdb6f",
+    userId: userIDs[6],
+    earnedPoints: 80,
+  },
+  {
+    id: "d869f5fc-a369-4d76-b42b-d14780504822",
+    userId: userIDs[7],
+    earnedPoints: 500,
+  },
+  {
+    id: "86a6d815-fcac-414c-b047-5ae6df81fc8b",
+    userId: userIDs[8],
+    earnedPoints: 160,
+  },
+];
