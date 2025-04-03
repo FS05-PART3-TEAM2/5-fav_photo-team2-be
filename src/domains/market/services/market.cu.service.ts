0b40d6ba810eadcb9e96297542974044c5662339
seha <hansaemj20@gmail.com>
2025-04-03 19:43:27 +0900

    refactor: 검증 로직 추가

--- src/domains/market/services/market.cu.service.ts
+++ src/domains/market/services/market.cu.service.ts
@@ -6,57 +6,59 @@ const createMarketItem: CreateMarketItem = async (body, userId) => {
   const { userPhotoCardId, quantity, price, exchangeOffer } = body;
   const { grade, genre, description } = exchangeOffer || {};
 
+  // 판매할 카드가 존재하는지 확인
   const userPhotoCard = await prisma.userPhotoCard.findUnique({
-    where: {
-      id: userPhotoCardId,
-    },
+    where: { id: userPhotoCardId },
   });
-  const saleCount = await prisma.saleCard.aggregate({
-    where: {
-      userPhotoCardId,
-      sellerId: userId,
-    },
-    _sum: {
-      quantity: true,
-    },
-  });
-
   if (!userPhotoCard || userPhotoCard.ownerId !== userId)
     throw new CustomError("Invalid user photo card", 400);
-  if (userPhotoCard.quantity - (saleCount._sum.quantity || 0) < quantity)
+
+  // 이미 판매중인 카드인지 확인
+  const onSaleCard = await prisma.saleCard.findFirst({
+    where: { userPhotoCardId, status: "ON_SALE" },
+  });
+  if (onSaleCard) throw new CustomError("Already on sale", 400);
+
+  // 판매할 카드의 수량이 충분한지 확인
+  if (userPhotoCard.quantity < quantity)
     throw new CustomError("Not enough quantity", 400);
 
-  const saleCard = await prisma.saleCard.create({
-    data: {
-      quantity,
-      price,
-      status: "ON_SALE",
-      exchangeDescription: description || "",
-      exchangeGrade: grade || "",
-      exchangeGenre: genre || "",
-      sellerId: userId,
-      photoCardId: userPhotoCard.photoCardId,
-      userPhotoCardId,
-    },
-    include: {
-      photoCard: true,
-      seller: {
-        select: {
-          nickname: true,
+  // === 트랜잭션으로 묶기 ===
+  const saleCard = await prisma.$transaction(async (tx) => {
+    const saleCard = await tx.saleCard.create({
+      data: {
+        remaining: quantity,
+        quantity,
+        price,
+        status: "ON_SALE",
+        exchangeDescription: description || "",
+        exchangeGrade: grade || "",
+        exchangeGenre: genre || "",
+        sellerId: userId,
+        photoCardId: userPhotoCard.photoCardId,
+        userPhotoCardId,
+      },
+      include: {
+        photoCard: true,
+        seller: {
+          select: { nickname: true },
         },
       },
-    },
+    });
+
+    await tx.marketOffer.create({
+      data: {
+        type: "SALE",
+        ownerId: userId,
+        saleCardId: saleCard.id,
+        exchangeOfferId: null,
+      },
+    });
+
+    return saleCard;
   });
 
-  await prisma.marketOffer.create({
-    data: {
-      type: "SALE",
-      ownerId: userId,
-      saleCardId: saleCard.id,
-      exchangeOfferId: null,
-    },
-  });
-
+  // === 결과 리턴 ===
   return {
     saleCardId: saleCard.id,
     userPhotoCardId: userPhotoCard.id,
