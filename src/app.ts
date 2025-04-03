5930280c6e2e78ffa3c24978ff8b65252275c9ca
seha <hansaemj20@gmail.com>
2025-04-03 16:57:41 +0900

    feat: cors 허용 추가

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,18 @@ import routes from "./domains/routes";
 import { swaggerUi, swaggerSpec } from "./utils/swagger/swagger";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/error.middleware";
+import cors from "cors";
 dotenv.config();
 const app = express();
+
+const corsOptions: cors.CorsOptions = {
+  origin: ["http://localhost:3000"], // 허용할 도메인
+  methods: ["GET", "POST", "PUT", "DELETE"], // 허용할 HTTP 메서드
+  credentials: true, // 쿠키 허용 여부
+};
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser());
 
 app.use(express.json());
