0ff0eb80d82ada1b6787f1533f80daafc43cd56b
seha <hansaemj20@gmail.com>
2025-04-07 16:18:32 +0900

    secure true

--- src/domains/auth/services/auth.service.ts
+++ src/domains/auth/services/auth.service.ts
@@ -108,7 +108,8 @@ export const loginService = async (data: LoginInput): Promise<AuthResponse> => {
       refreshToken: refreshToken,
       options: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        // secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "none",
         maxAge: 60 * 60 * 1000,
       },
