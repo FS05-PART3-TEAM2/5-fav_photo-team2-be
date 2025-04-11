561d0af7ec63a1e0eff88a4cad699671c186b879
민지영 <kuck523@gmail.com>
2025-04-11 18:36:46 +0900

    next plz

--- src/domains/photocards/photocard.routes.ts
+++ src/domains/photocards/photocard.routes.ts
@@ -28,12 +28,21 @@ router.get("/me/count", authenticate, requestHandler(getMyPhotocardsCount));
 // 포토카드 생성 라우트
 router.post(
   "/",
-  () => console.log("포토카드 생성 라우트"),
-  authenticate, // 인증 미들웨어
-  () => console.log("인증 완료"),
-  upload.single("image"), // 파일 업로드 (req.file 생성)
-  () => console.log("파일 파싱 완료"),
-  requestHandler(createPhotocard) // 실제 서비스 실행
+  (req, res, next) => {
+    console.log("포토카드 생성 라우트");
+    next();
+  },
+  authenticate,
+  (req, res, next) => {
+    console.log("인증 완료");
+    next();
+  },
+  upload.single("image"),
+  (req, res, next) => {
+    console.log("파일 파싱 완료");
+    next();
+  },
+  requestHandler(createPhotocard)
 );
 
 //내 포토카드 상세조회
